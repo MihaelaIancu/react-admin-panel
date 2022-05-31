d589f1a0c58596f06b02e608570982e106fc00c5
MihaelaIancu <iancumihaela46@yahoo.com>
2022-05-31 19:43:16 +0300

    Updates

---
+++ precache-manifest.bea6fd7d79a54d84b4dabf0ec2c1d9b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02707be88dc927698f14d6745554cd22",
+    "url": "/react-project-1/index.html"
+  },
+  {
+    "revision": "498be66535c4cfac169f",
+    "url": "/react-project-1/static/css/main.df6f41ea.chunk.css"
+  },
+  {
+    "revision": "ca2cf63d09cc1af1b57e",
+    "url": "/react-project-1/static/js/2.32c4571d.chunk.js"
+  },
+  {
+    "revision": "498be66535c4cfac169f",
+    "url": "/react-project-1/static/js/main.24d9bdd4.chunk.js"
+  },
+  {
+    "revision": "e5808e52b032d405946a",
+    "url": "/react-project-1/static/js/runtime-main.e38cca8f.js"
+  }
+]);
